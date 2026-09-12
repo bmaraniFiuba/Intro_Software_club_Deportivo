82d7e907ee426f1f752318348d262276b2191007
baltazar Marani <bmarani@fi.uba.ar>
2026-09-12 14:45:42 -0300

    corrigo errore de sintaxis para que ande y agrego id_cancha como PM

--- init_db.sql
+++ init_db.sql
@@ -1,7 +1,7 @@
 -- OREGUNTAR SI HACE FALTA PONER CREATE...EXIXST PARA CADA TABLA
 -- VER FRANJA HORARIA
-
-CREATE DATABASE club_Deportivo IF NOT EXISTS club_Deportivo;
+--DROP DATABASE IF exists club_Deportivo;
+CREATE DATABASE IF NOT EXISTS club_Deportivo;
 
 USE club_Deportivo;
 
@@ -11,35 +11,36 @@ CREATE TABLE Deportes (
 );
 
 CREATE TABLE Canchas (
-    idDeporte int(2),
-    nombre varchar(20) PRIMARY KEY,
-    Disponibilidad boolean, --si alguien la reservo o no
-    precio_hora int (10),
-    Techada boolean DEFAULT false, --VEMOS
-    Activa boolean DEFAULT true -- si se puede usar o no 
-    Horario_inicio Time(8), --ver
-    Horario_fin Time (23) --ver
+    id_cancha int PRIMARY KEY,
+    idDeporte int,
+    nombre_cancha varchar(20),
+    Disponibilidad boolean, -- si alguien la reservo o no
+    precio_hora int,
+    Techada boolean DEFAULT false, -- VEMOS
+    Activa boolean DEFAULT true, -- si se puede usar o no 
+    Horario_inicio Time, -- ver
+    Horario_fin Time -- ver
 );
 
 
-CREATE TABLE Socio (
+CREATE TABLE Socios (
     id_socio INT AUTO_INCREMENT PRIMARY KEY,
     nombre varchar(50),
     mail varchar(50), -- falta exp reg para probar validez
-    activo boolean DEFAULT true,
+    activo boolean DEFAULT true
 );
 
 CREATE TABLE Reservas (
     numero_reserva INT AUTO_INCREMENT PRIMARY KEY,
-    id_socio, -- NO VACIO
-    nombre_cancha, -- MO VACIO
+    id_socio INT, -- NO VACIO
+    nombre_cancha varchar(20), -- MO VACIO
     estado varchar(20),
     fecha_desde DATETIME,
     fecha_hasta DATETIME,
 
 
-    FOREIGN KEY (id_socio) REFERENCES socios(id_socio),
-    FOREIGN KEY (id_cancha) REFERENCES canchas(nombre_cancha)
+    FOREIGN KEY (id_socio) REFERENCES Socios(id_socio),
+    FOREIGN KEY (id_cancha) REFERENCES Canchas(id_cancha)
 );
 
 
