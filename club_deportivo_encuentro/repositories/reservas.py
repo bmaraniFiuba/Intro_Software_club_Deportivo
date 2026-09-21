cd157af95967959c3ddcc81e1475c53177aba15d
baltazar Marani <bmarani@fi.uba.ar>
2026-09-21 17:52:29 -0300

    termino la funcion obtenerReservas

--- club_deportivo_encuentro/repositories/reservas.py
+++ club_deportivo_encuentro/repositories/reservas.py
@@ -5,17 +5,20 @@ def obtener_reservas (filtros: dict, offset:int, limit:int) -> list[dict]:
     cond = []
     query_params = {}
     
-    if filtros.get('id_socio') in not None:
+# :algo es un parámetro que después se completa con el valor
+# correspondiente dentro de query_params.
+    
+    if filtros.get('id_socio') is not None:
         cond.append("id_socio = :id_socio")
         query_params["id_socio"] = filtros["id_socio"]
     
 
-    if filtros.get('id_cancha') in not None:
+    if filtros.get('id_cancha') is not None:
         cond.append("id_cancha = :id_cancha")
         query_params["id_cancha"] = filtros["id_cancha"]
 
 
-    if filtros.get('estado') in not None:
+    if filtros.get('estado') is not None:
         cond.append("estado = :estado")
         query_params["estado"] = filtros["estado"]
     
@@ -29,9 +32,16 @@ def obtener_reservas (filtros: dict, offset:int, limit:int) -> list[dict]:
         
     where = ""
     if len(cond) > 0:
-        where = "Where"
+        where = " Where "
         for condicion in range (len(cond)):
-            where += cond[i]
-            if condicion < len(cond) - 1: ## chequeo si es el ultimo elemento de la lista cond
-                where = "AND" +
+            where += cond[condicion]
+            if condicion < len(cond) - 1: ## si no es el ultimo elemento de la lista, etra a este if
+                where += ' AND '
+    
+        SQL = f"SELECT * FROM reservas {where} ORDER BY id LIMIT :limit OFFSET :offset"
+        #cuando esta funcion recibe limit y offset como parámetros, ya le llegan validados y con su valor definitivo
+        query_params["limit"] = limit
+        query_params["offset"] = offset 
+    return (SQL, query_params) # sqlalchemy agarra :id_socio y lo relaciona con ej "id_socio": 5
+    
                 
