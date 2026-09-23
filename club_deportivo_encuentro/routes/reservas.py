612a02fa1d04537e809e22df7e53572792a362a5
Lucía <lhernandezt@fi.uba.ar>
2026-09-23 02:14:50 -0300

    defino endpoint get reserva id

--- club_deportivo_encuentro/routes/reservas.py
+++ club_deportivo_encuentro/routes/reservas.py
@@ -38,7 +38,13 @@ def post_reserva():
 
 @reservas_bp.route('/reservas/<int:id_reserva>', methods=['GET'])
 def get_reserva_id(id_reserva):
-    return
+    try:
+        reserva = obtener_reserva_por_id(id_reserva)
+    except ValueError as error:
+        status = error.args[1] if len(error.args) > 1 else 400
+        return jsonify(error.args[0]), status
+
+    return jsonify(reserva), 200
 
 @reservas_bp.route('/reservas/<int:id_reserva>/estado', methods=['PUT'])
 def put_reserva_estado(id_reserva):
